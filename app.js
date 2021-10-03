3c425af4196dd39f983d847c359f3d36285cbe1e
tommy <tommysolano18@hotmail.com>
2021-10-03 02:30:31 -0500

    listing product in screen

---
+++ app.js
@@ -0,0 +1,48 @@
+class Product {
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI {
+    addProduct(product){
+        const productList = document.getElementById("product-list")
+        const element = document.createElement("div");
+        element.innerHTML = `
+        <div class="card text-center mb-4">
+            <div class="card-body">
+                <strong>Product</strong>: ${product.name}
+                <strong>Product Price</strong>: ${product.price}
+                <strong>Product Year</strong>: ${product.year}
+            </div>
+        </div>
+        `;
+        productList.appendChild(element)
+    }
+
+    deleteProduct(){
+
+    }
+
+    showMessage(){
+
+    }
+}
+
+//DOM event
+
+document.getElementById("product-form")
+    .addEventListener("submit", function(e) {
+        const name = document.getElementById("name").value
+        const price = document.getElementById("price").value
+        const year = document.getElementById("year").value
+        
+        const product = new Product(name, price, year)
+        
+        const ui = new UI()
+        ui.addProduct(product)
+
+        e.preventDefault();
+    })
